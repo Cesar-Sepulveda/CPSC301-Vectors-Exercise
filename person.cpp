1c9fd84eb5b94828bf25f3999d009a3211d3420b
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-02-28 19:14:03 -0800

    Create person.cpp

---
+++ person.cpp
@@ -0,0 +1,70 @@
+#include "person.h"
+#include <string>
+
+using namespace std;
+
+Person::Person(){
+  payRate = 0.0;
+  hoursWorked = 0.0;
+  firstName = "No";
+  lastName = "employee";
+  employeeID = 0;
+  companyName = "No company";
+}
+
+void Person::setFirstName(string fName){
+  firstName = fName;
+}
+
+string Person::getFirstName() {
+    return firstName;
+}
+
+void Person::setLastName(string lName){
+  lastName = lName;
+}
+
+string Person::getLastName() {
+    return lastName;
+}
+
+void Person::setEmployeeId(int id){
+  employeeID = id;
+}
+
+int Person::getEmployeeId(){
+  return employeeID;
+}
+
+void Person::setCompanyName(string coName){
+  companyName = coName;
+}
+
+string Person::getCompanyName(){
+  return companyName;
+}
+
+void Person::setPayRate(float rate){
+  payRate = rate;
+}
+
+float Person::getPayRate(){
+  return payRate;
+}
+
+void Person::setHoursWorked(float hours){
+  hoursWorked = hours;
+}
+
+float Person::getHoursWorked(){
+  return hoursWorked;
+}
+
+float Person::totalPay(){
+  return payRate*hoursWorked;
+}
+
+string Person::fullName(){
+  string full = firstName + " " + lastName;
+  return full;
+}
